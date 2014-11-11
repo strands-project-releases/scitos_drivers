cbea827f4c79f05460be5b94e69e6a8333d0d5e7
Marc Hanheide <marc@hanheide.de>
2014-11-11 19:57:15 +0000

    Revert "Trimmed the branch to only the scitos_drivers sub directory"
    This reverts commit f0bd7c16828cd8688e194267315abfe0e1b23c35.

---
+++ scitos_mira/include/scitos_mira/ScitosG5.h
@@ -0,0 +1,42 @@
+/* ScitosG5.h
+ *      Author: chris burbridge
+ *
+ * Main class for the robot. Instantiates modules present on the robot, manages
+ * MIRA and ROS handles.
+ * 
+ */
+
+#ifndef SCITOSG5_H
+#define SCITOSG5_H
+
+#include <ros/ros.h>
+#include <tf/transform_broadcaster.h>
+#include <fw/Framework.h>
+#include <scitos_mira/ScitosModule.h>
+#include <vector>
+#include <string>
+
+class ScitosG5{
+public:
+  ScitosG5(std::vector<std::string> modules);
+  ~ScitosG5();
+
+  mira::Authority& getMiraAuthority();
+  ros::NodeHandle& getRosNode();
+
+  void initialize();
+  void spin();
+
+  void registerSpinFunction(boost::function<void ()> function);
+  tf::TransformBroadcaster& getTFBroadcaster();
+
+private:
+  mira::Authority authority_;
+  tf::TransformBroadcaster tf_broadcaster_;
+  ros::NodeHandle node_;
+  std::vector<ScitosModule*> modules_;
+  std::vector< boost::function<void ()> > spin_functions_;
+
+};
+
+#endif
