1920adb91c2a20c1befa4bcff9715eca36473a8c
Chris Burbridge <cburbridge@gmail.com>
2015-01-13 14:19:32 +0000

    Revert "Trimmed the branch to only the flir_pantilt_d46 sub directory"
    This reverts commit 8c219057217662e8baab624e02c6a545e4a9efd9.

---
+++ scitos_mira/src/ScitosEBC.cpp
@@ -0,0 +1,73 @@
+#include "scitos_mira/ScitosEBC.h"
+
+
+ScitosEBC::ScitosEBC() : ScitosModule(std::string ("EBC")), reconfigure_srv_(name_) {
+
+}
+
+void ScitosEBC::initialize() {
+	reconfigure_srv_.setCallback(boost::bind(&ScitosEBC::reconfigure_callback, this, _1, _2));
+
+}
+
+void ScitosEBC::reconfigure_callback( scitos_mira::EBCParametersConfig& config, uint32_t level) {
+	ROS_DEBUG("Reconfigure request on ScitosEBC module.");
+	//Set the MIRA parameters to what was selected...
+	// Port MCU 
+	if (config.MCU_5V_Enabled)
+	  set_mira_param_("MainControlUnit.EBC_5V.Enabled","true");
+	else
+	  set_mira_param_("MainControlUnit.EBC_5V.Enabled","false");
+	set_mira_param_("MainControlUnit.EBC_5V.MaxCurrent",std::to_string(config.MCU_5V_MaxCurrent));
+				   
+	if (config.MCU_12V_Enabled)
+	  set_mira_param_("MainControlUnit.EBC_12V.Enabled","true");
+	else
+	  set_mira_param_("MainControlUnit.EBC_12V.Enabled","false");
+	set_mira_param_("MainControlUnit.EBC_12V.MaxCurrent",std::to_string(config.MCU_12V_MaxCurrent));
+	
+	if (config.MCU_24V_Enabled)
+	  set_mira_param_("MainControlUnit.EBC_24V.Enabled","true");
+	else
+	  set_mira_param_("MainControlUnit.EBC_24V.Enabled","false");
+	set_mira_param_("MainControlUnit.EBC_24V.MaxCurrent",std::to_string(config.MCU_24V_MaxCurrent));
+
+	// Port 0 
+	if (config.Port0_5V_Enabled)
+	  set_mira_param_("EBC7.Port0_5V.Enabled","true");
+	else
+	  set_mira_param_("EBC7.Port0_5V.Enabled","false");
+	set_mira_param_("EBC7.Port0_5V.MaxCurrent",std::to_string(config.Port0_5V_MaxCurrent));
+				   
+	if (config.Port0_12V_Enabled)
+	  set_mira_param_("EBC7.Port0_12V.Enabled","true");
+	else
+	  set_mira_param_("EBC7.Port0_12V.Enabled","false");
+	set_mira_param_("EBC7.Port0_12V.MaxCurrent",std::to_string(config.Port0_12V_MaxCurrent));
+	
+	if (config.Port0_24V_Enabled)
+	  set_mira_param_("EBC7.Port0_24V.Enabled","true");
+	else
+	  set_mira_param_("EBC7.Port0_24V.Enabled","false");
+	set_mira_param_("EBC7.Port0_24V.MaxCurrent",std::to_string(config.Port0_24V_MaxCurrent));
+
+	// Port 1
+	if (config.Port1_5V_Enabled)
+	  set_mira_param_("EBC7.Port1_5V.Enabled","true");
+	else
+	  set_mira_param_("EBC7.Port1_5V.Enabled","false");
+	set_mira_param_("EBC7.Port1_5V.MaxCurrent",std::to_string(config.Port1_5V_MaxCurrent));
+	
+	if (config.Port1_12V_Enabled)
+	  set_mira_param_("EBC7.Port1_12V.Enabled","true");
+	else
+	  set_mira_param_("EBC7.Port1_12V.Enabled","false");
+	set_mira_param_("EBC7.Port1_12V.MaxCurrent",std::to_string(config.Port1_12V_MaxCurrent));
+
+	if (config.Port1_24V_Enabled)
+	  set_mira_param_("EBC7.Port1_24V.Enabled","true");
+	else
+	  set_mira_param_("EBC7.Port1_24V.Enabled","false");
+	set_mira_param_("EBC7.Port1_24V.MaxCurrent",std::to_string(config.Port1_24V_MaxCurrent));
+
+}
