5d0a870233d6041f20be7721d7f7790ce441dc84
Chris Burbridge <cburbridge@gmail.com>
2015-01-13 14:19:33 +0000

    Revert "Trimmed the branch to only the scitos_bringup sub directory"
    This reverts commit 84266a652197fc8f350241bcef19babfa1c4f32d.

---
+++ scitos_mira/include/scitos_mira/ScitosModule.h
@@ -0,0 +1,36 @@
+/* ScitosModule.h
+ *      Author: chris burbridge
+ *
+ * Base class for all Scitos modules (Head, EBC, Drive etc)
+ */
+
+#ifndef SCITOSMODULE_H_
+#define SCITOSMODULE_H_
+
+#include <ros/ros.h>
+#include <fw/Framework.h>
+#include <string>
+
+class ScitosG5;
+
+class ScitosModule {
+public:
+  ScitosModule(std::string name);
+	void setRobot(ScitosG5 *robot) { robot_ = robot; };
+
+	virtual void initialize() = 0;
+
+	virtual ~ScitosModule();
+
+protected:
+	bool set_mira_param_(std::string param_name, std::string value);
+	std::string get_mira_param_(std::string param_name);
+  ScitosG5 *robot_;
+  std::string name_;
+  ros::NodeHandle module_handle_;
+
+};
+
+typedef ScitosModule* (*ModuleCreator)(void);
+
+#endif /* SCITOSMODULE_H_ */
