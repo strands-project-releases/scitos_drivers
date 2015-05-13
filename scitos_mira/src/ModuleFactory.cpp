7a314bb20fbadd3f33d00352dd5a85045f8b29a2
STRANDS Jenkins <strands@hanheide.net>
2015-05-13 15:32:01 +0000

    Revert "Trimmed the branch to only the flir_pantilt_d46 sub directory"
    This reverts commit 6e1be57af657ed1074bed108405c5d7014b8bc9d.

---
+++ scitos_mira/src/ModuleFactory.cpp
@@ -0,0 +1,38 @@
+#include "scitos_mira/ModuleFactory.h"
+
+#include "scitos_mira/ScitosCharger.h"
+#include "scitos_mira/ScitosDisplay.h"
+#include "scitos_mira/ScitosDrive.h"
+#include "scitos_mira/ScitosEBC.h"
+#include "scitos_mira/ScitosHead.h"
+
+ModuleFactory::ModuleFactory() {
+	Register("Charger", &ScitosCharger::Create);
+	Register("Display", &ScitosDisplay::Create);
+	Register("Drive", &ScitosDrive::Create);
+	Register("EBC", &ScitosEBC::Create);
+	Register("Head", &ScitosHead::Create);
+}
+
+void ModuleFactory::Register(const std::string &name, ModuleCreator func) {
+	modules_[name] = func;
+}
+
+ScitosModule *ModuleFactory::CreateModule(std::string name, ScitosG5 *robot) {
+  std::map<std::string, ModuleCreator>::iterator it = modules_.find(name);
+  if (it != modules_.end()) {
+    ScitosModule *mod = it->second();
+    mod->setRobot(robot);
+    return mod;
+  }
+  ROS_ERROR("Trying to create unknown Scitos module.");
+  return NULL;
+}
+
+bool ModuleFactory::CheckForModule(const std::string &name) {
+  std::map<std::string, ModuleCreator>::iterator it = modules_.find(name);
+  if (it != modules_.end()) {
+    return true;
+  }
+  return false;
+}
